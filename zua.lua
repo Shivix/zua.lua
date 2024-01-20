f63a0ff154feb41da04e6c66ec21fde90bea49d4
Mark Oborne <markoborne1@gmail.com>
2024-01-20 20:19:41 +0100

    chore: simplified and optimized

--- zua.lua
+++ zua.lua
@@ -3,12 +3,6 @@
 DATA_FILE = "~/.local/state/zua/data"
 DATA_FILE = DATA_FILE:gsub("^~", assert(os.getenv("HOME")))
 
-local function is_directory(path)
-    path = path:gsub("^~", assert(os.getenv("HOME")))
-    local is_dir = os.execute("test -d '" .. path .. "'") or false
-    return is_dir
-end
-
 if #arg == 0 then
     print("cd")
 end
@@ -21,6 +15,10 @@ end
 if arg[1] == "--init" then
     print([[
 function _zua_cd
+    if test "$argv" = -; or test "$argv" = ..
+        cd $argv
+        return
+    end
     eval (zua.lua $argv)
 end
 function _zua_add --on-variable PWD
@@ -31,7 +29,6 @@ alias z _zua_cd
     os.exit(0)
 end
 
--- TODO: Add to top of file when CD'd and delete bottom every so often?
 if arg[1] == "--add" then
     local file = io.open(DATA_FILE, "r")
     if file == nil then
@@ -54,18 +51,16 @@ if data == nil then
     error("data_file does not exist")
 end
 
-if #arg == 2 and arg[1] == "-" or arg[1] == ".." or is_directory(arg[1]) then
-    print("cd " .. arg[1])
-    os.exit(0)
+for i, a in ipairs(arg) do
+    arg[i] = a:lower()
 end
 
 for line in data:lines() do
     for _, a in ipairs(arg) do
-        if not line:lower():find(a:lower()) then
+        if not line:lower():find(a) then
             goto next_line
         end
     end
-    line = line:gsub("^~", assert(os.getenv("HOME")))
     print("cd " .. line)
     os.exit(0)
     ::next_line::
