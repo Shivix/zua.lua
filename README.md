c977aee00e41d06d354001f9f976156bcba64a9c
Mark Oborne <markoborne1@gmail.com>
2024-06-11 23:56:20 +0100

    feat: Add --case and --patternmatch args
    Adds a far more robust args system to increase usability.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Zua.lua
+
+A simple, lightweight and predictable autojump tool.
+
+## Usage
+Install zua.lua somewhere your shell can find it. A Makefile is provided that installs it to /usr/local/bin.
+
+Add the following line to your shell config:
+```bash
+zua.lua --init | source
+```
+
+Supports opt in pattern matching on file matching and opt in case sensitivity.\
+See zua --help for details.\
+`$ZUA_DEFAULT_ARGS` Environment variable may be set to adjust default behaviour.
+
+## Issues
+Any bugs/ requests can be added to the [issues](https://github.com/Shivix/prefix/issues) page on the GitHub repository.\
+Note that feature requests will be highly but fairly scrutinized. The number one focus here is to be simple and lightweight.
--- zua.lua
+++ zua.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/env lua
+-- If you'd like to use luajit or Lua51 etc, simply change the shebang
 
 DATA_FILE = "~/.local/state/zua/data"
 DATA_FILE = DATA_FILE:gsub("^~", assert(os.getenv("HOME")))
@@ -7,61 +8,130 @@ if #arg == 0 then
     print("cd")
 end
 
-if arg[1] == "--edit" then
-    print(os.getenv("EDITOR") .. " " .. DATA_FILE)
-    os.exit(0)
-end
+local args = {
+    add = false,
+    case = false,
+    edit = false,
+    init = false,
+    help = false,
+    patternmatch = false,
+}
+local patterns = {}
+local help_msg = [[
+zua 1.0.1
+A simple and lightweight autojump tool
 
-if arg[1] == "--init" then
+USAGE:
+    zua [OPTIONS] [pattern]...
+
+ARGS:
+    <pattern>...   A set of patterns that must all match part of a path.
+
+
+OPTIONS:
+    --add          Adds the provided path to the data file.
+    --case         Make the pattern case sensitive.
+    --edit         Open up the data file in $EDITOR.
+    --init         Outputs the required shell code to be added to shell config.
+                   (Only Fish supported currently)
+    --help         Prints help information.
+    --patternmatch By default zua will escape ( ) . % + - * ? [ ^ $ and match these literally.
+                   This option will disable this and utilise Lua pattern matching.
+
+ENVIRONMENT VARIABLES:
+       ZUA_DEFAULT_ARGS
+           Specifies the args to be provided every time zua is called through the shell function
+]]
+
+local function initialize()
     print([[
 function _zua_cd
     if test "$argv" = -; or test "$argv" = ..
         cd $argv
         return
     end
-    eval (zua.lua $argv)
+    if test -z "$argv"
+        cd
+        return
+    end
+    eval (zua.lua $argv $ZUA_DEFAULT_ARGS)
 end
 function _zua_add --on-variable PWD
     zua.lua --add $PWD
 end
 alias z _zua_cd
-    ]])
-    os.exit(0)
+]])
 end
 
-if arg[1] == "--add" then
-    local file = io.open(DATA_FILE, "r")
-    if file == nil then
-        os.exit(1)
+local function add_path()
+    local data = io.open(DATA_FILE, "r")
+    if data == nil then
+        error("data_file does not exist")
     end
-    for line in file:lines() do
-        if line == arg[2] then
-            os.exit(0)
+    if args.add then
+        assert(#patterns == 1)
+        local path = patterns[1]
+        for line in data:lines() do
+            if line == path then
+                return
+            end
+        end
+        data:close()
+        data = assert(io.open(DATA_FILE, "a+"))
+        data:write(path .. "\n")
+    end
+end
+
+local function find_match()
+    local data = io.open(DATA_FILE, "r")
+    if data == nil then
+        error("data_file does not exist")
+    end
+
+    if not args.case then
+        for i, pattern in ipairs(patterns) do
+            patterns[i] = pattern:lower()
         end
     end
-    file:close()
-    file = assert(io.open(DATA_FILE, "a+"))
-    file:write(arg[2] .. "\n")
-    os.exit(0)
-end
 
-local data = io.open(DATA_FILE, "r")
-
-if data == nil then
-    error("data_file does not exist")
-end
-
-for i, a in ipairs(arg) do
-    arg[i] = a:lower()
-end
-
-for line in data:lines() do
-    for _, a in ipairs(arg) do
-        if not line:lower():find(a) then
-            goto next_line
+    for line in data:lines() do
+        for _, a in ipairs(patterns) do
+            if not args.patternmatch then
+                -- Escape the magic characters and match them literally.
+                a = a:gsub("([%(%)%.%%%+%-%*%?%[%]%^%$])", "%%%1")
+            end
+            if not line:lower():find(a) then
+                goto continue
+            end
         end
+        print("cd " .. line)
+        os.exit()
+        ::continue::
     end
-    print("cd " .. line)
-    os.exit(0)
-    ::next_line::
+end
+
+for _, a in ipairs(arg) do
+    local pos = assert(a:find("[^-]"))
+    if pos ~= 1 then
+        local name = a:sub(pos, -1)
+        if args[name] == nil then
+            io.stderr:write(help_msg)
+            os.exit(1)
+        end
+        args[name] = true
+    else
+        table.insert(patterns, a)
+    end
+end
+
+if args.help then
+    print(help_msg)
+elseif args.edit then
+    print(os.getenv("EDITOR") .. " " .. DATA_FILE)
+elseif args.add then
+    add_path()
+elseif args.init then
+    initialize()
+else
+    find_match()
 end
