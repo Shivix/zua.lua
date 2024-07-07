153087b52a8b9a2f7daf0dcd383687049887977f
Mark Oborne <markoborne1@gmail.com>
2024-07-07 21:25:47 +0100

    feat!: release v1.1.0
    Reworks the args system to more intuitive cmds
    Ensures eval will only ever run cd
    Makes the data file location configurable
    Adds support for ZSH
    Add a / to the end of PWD when auto adding paths after CD so that `dir/`
    will match that dir better

--- README.md
+++ README.md
@@ -5,20 +5,24 @@ A simple, lightweight and predictable autojump tool.
 ## Usage
 Install zua.lua somewhere your shell can find it. A Makefile is provided that installs it to /usr/local/bin.
 
-Add the following line to your shell config:
+Add one of the following lines to your shell config:
 ```bash
-zua.lua --init | source
+# Fish
+zua.lua init fish | source
+# Zsh
+source <(zua.lua init zsh)
 ```
 
 Paths will be added to the data file whenever you `cd` into a new directory.\
 Then you can give zua a pattern or number of patterns and zua will cd into the first path that matches all patterns.
 ```bash
-z <pattern>
+$ z <pattern>
 ```
 
 Supports opt in pattern matching on file matching and opt in case sensitivity.\
 See `zua.lua --help` for details.\
-`$ZUA_DEFAULT_ARGS` Environment variable may be set to adjust default behaviour.
+`$ZUA_DEFAULT_ARGS` Environment variable may be set to adjust default behaviour.\
+`$ZUA_DATA_FILE` Environment variable may be set to adjust where the paths are stored.
 
 ## Issues
 Any bugs/ requests can be added to the [issues](https://github.com/Shivix/prefix/issues) page on the GitHub repository.\
--- zua.lua
+++ zua.lua
@@ -1,53 +1,56 @@
 #!/usr/bin/env lua
 -- If you'd like to use luajit or Lua51 etc, simply change the shebang
 
-DATA_FILE = "~/.local/state/zua/data"
+DATA_FILE = os.getenv("ZUA_DATA_FILE") or "~/.local/state/zua/data"
 DATA_FILE = DATA_FILE:gsub("^~", assert(os.getenv("HOME")))
 
-if #arg == 0 then
-    print("cd")
-end
-
 local args = {
-    add = false,
     case = false,
-    edit = false,
-    init = false,
     help = false,
     patternmatch = false,
     version = false,
 }
 local patterns = {}
-local version = "1.0.2"
+local version = "1.1.0"
 local help_msg = [[
-zua ]] .. version .. [[
+zua.lua ]] .. version .. [[
 
 A simple and lightweight autojump tool
 
 USAGE:
-    zua [OPTIONS] [pattern]...
+    If the shell has been configured then:
+    z <pattern>...   This will match the pattern against the paths contained with $ZUA_DATA_FILE
+                     and jump to the first match
+    If using zua directly then:
+    zua.lua [CMD] [ARGS]
+
+CMD:
+    add <path>       Adds the provided path to the data file.
+    init <shell>     Outputs the required shell code to be added to shell config.
+                     "fish" and "zsh" are currently supported.
+    jump             Matches the patterns to a path prints a cd command for that path.
+    edit             Open up the data file in $EDITOR.
 
 ARGS:
-    <pattern>...   A set of patterns that must all match part of a path.
-
-
-OPTIONS:
-    --add          Adds the provided path to the data file.
-    --case         Make the pattern case sensitive.
-    --edit         Open up the data file in $EDITOR.
-    --init         Outputs the required shell code to be added to shell config.
-                   Only Fish supported currently
-    --help         Prints help information.
-    --patternmatch By default zua will escape ( ) . % + - * ? [ ^ $ and match these literally.
-                   This option will disable this and utilise Lua pattern matching.
+    --case           Make the pattern case sensitive.
+    --help           Prints help information.
+    --patternmatch   By default zua will escape ( ) . % + - * ? [ ^ $ and match these literally.
+                     This option will disable this and utilise Lua pattern matching.
 
 ENVIRONMENT VARIABLES:
        ZUA_DEFAULT_ARGS
-           Specifies the args to be provided every time zua is called through the shell function
+           Specifies the args to be provided every time zua is called through the shell function.
+       ZUA_DATA_FILE
+           Specifies the path to the data file to store paths in.
+
+https://github.com/Shivix/zua.lua
 ]]
 
 local function initialize()
-    print([[
+    assert(#patterns == 1)
+    local shell = patterns[1]
+    if shell == "fish" then
+        print([[
 function _zua_cd
     if test "$argv" = -; or test "$argv" = ..
         cd $argv
@@ -57,38 +60,61 @@ function _zua_cd
         cd
         return
     end
-    eval (zua.lua $argv $ZUA_DEFAULT_ARGS)
+    eval (zua.lua jump $argv $ZUA_DEFAULT_ARGS)
+    pwd
 end
 function _zua_add --on-variable PWD
-    zua.lua --add $PWD
+    zua.lua add $PWD/
 end
 alias z _zua_cd
+set -gx ZUA_DATA_FILE $HOME/.local/state/zua/data
 ]])
+    elseif shell == "zsh" then
+        print([[
+_zua_cd() {
+    if [[ "$@" = "-" || "$@" = ".." ]].."]]"..[[; then
+        cd "$@"
+        return
+    fi
+    if [[ -z "$@" ]].."]]"..[[; then
+        cd
+        return
+    fi
+    eval $(zua.lua jump $@ $ZUA_DEFAULT_ARGS)
+}
+_zua_add() {
+    zua.lua add $PWD/
+}
+chpwd_functions+=(_zua_add)
+alias z="_zua_cd"
+export ZUA_DATA_FILE="$HOME/.local/state/zua/data"
+]])
+    else
+        error("shell not supported: " .. shell)
+    end
 end
 
 local function add_path()
     local data = io.open(DATA_FILE, "r")
     if data == nil then
-        error("data_file does not exist")
+        error("file at $ZUA_DATA_FILE does not exist")
     end
-    if args.add then
-        assert(#patterns == 1)
-        local path = patterns[1]
-        for line in data:lines() do
-            if line == path then
-                return
-            end
+    assert(#patterns == 1)
+    local path = patterns[1]
+    for line in data:lines() do
+        if line == path then
+            return
         end
-        data:close()
-        data = assert(io.open(DATA_FILE, "a+"))
-        data:write(path .. "\n")
     end
+    data:close()
+    data = assert(io.open(DATA_FILE, "a+"))
+    data:write(path .. "\n")
 end
 
 local function find_match()
     local data = io.open(DATA_FILE, "r")
     if data == nil then
-        error("data_file does not exist")
+        error("file at $ZUA_DATA_FILE does not exist")
     end
 
     if not args.case then
@@ -107,36 +133,45 @@ local function find_match()
                 goto continue
             end
         end
-        print("cd " .. line)
+        -- Ensure that we only ever try to cd with a single string arg to avoid eval running any bad code.
+        print("cd '" .. line:gsub("'", "'\\''") .. "'")
         os.exit()
         ::continue::
     end
 end
 
-for _, a in ipairs(arg) do
-    local pos = assert(a:find("[^-]"))
-    if pos ~= 1 then
-        local name = a:sub(pos, -1)
+if #arg == 0 then
+    io.stderr:write(help_msg)
+    os.exit(1)
+end
+
+local cmd = arg[1]
+for i = 2, #arg do
+    local pos = assert(arg[i]:find("[^-]"))
+    if pos > 1 then
+        local name = arg[i]:sub(pos, -1)
         if args[name] == nil then
-            io.stderr:write(help_msg)
+            io.stderr:write(name)
             os.exit(1)
         end
         args[name] = true
     else
-        table.insert(patterns, a)
+        table.insert(patterns, arg[i])
     end
 end
 
 if args.help then
     io.stderr:write(help_msg)
 elseif args.version then
-    print("echo zua v" .. version)
-elseif args.edit then
-    print(os.getenv("EDITOR") .. " " .. DATA_FILE)
-elseif args.add then
+    print("echo zua.lua v" .. version)
+elseif cmd == "add" then
     add_path()
-elseif args.init then
+elseif cmd == "edit" then
+    print(os.getenv("EDITOR") .. " " .. DATA_FILE)
+elseif cmd == "init" then
     initialize()
-else
+elseif cmd == "jump" then
     find_match()
+else
+    io.stderr:write(help_msg)
 end
